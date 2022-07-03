a453059fa59f8e02bf0b5ffb7fdb26e61494a234
Abhi Barkade <abhibarkade.projects@gmail.com>
2022-07-03 12:24:41 -0700

    calculate lecture length

--- app/src/main/java/com/abhibarkadde/coursezila/course/helper/LectureListAdapter.java
+++ app/src/main/java/com/abhibarkadde/coursezila/course/helper/LectureListAdapter.java
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,8 @@ import com.abhibarkadde.coursezila.R;
 import com.abhibarkadde.coursezila.course.FullScreenPlayer;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class LectureListAdapter extends RecyclerView.Adapter<LectureListAdapter.ViewHolder> {
@@ -37,29 +42,44 @@ public class LectureListAdapter extends RecyclerView.Adapter<LectureListAdapter.
         return new ViewHolder(view);
     }
 
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.name.setText(list.get(position).split("~")[0]);
         holder.root.setOnClickListener(v -> activity.startActivity(new Intent(activity, FullScreenPlayer.class)
                 .putExtra("Url", list.get(position).split("~")[1])));
-        holder.time.setText(getLectureTime(list.get(position).split("~")[1]));
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            try {
+                String url = list.get(position).split("~")[1].trim();
+                MediaPlayer mp = MediaPlayer.create(activity, Uri.parse(url));
+                int duration = mp.getDuration();
+                mp.release();
+                holder.time.setText(String.format("%d min %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes(duration),
+                        TimeUnit.MILLISECONDS.toSeconds(duration) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
+                ));
+                handler.post(() -> {
+                    //Toast.makeText(activity, "HIT", Toast.LENGTH_SHORT).show();
+                    // update the result to the UI thread
+                    // or any operation you want to perform on UI thread. It is similar to onPostExecute() of AsyncTask
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+                handler.post(() -> {
+
+                });
+            }
+
+        });
     }
 
     @Override
     public int getItemCount() {
         return list.size();
-    }
-
-    @SuppressLint("DefaultLocale")
-    public String getLectureTime(String url) {
-        MediaPlayer mp = MediaPlayer.create(activity, Uri.parse(url.trim()));
-        int duration = mp.getDuration();
-        mp.release();
-        return String.format("%d min %d sec",
-                TimeUnit.MILLISECONDS.toMinutes(duration),
-                TimeUnit.MILLISECONDS.toSeconds(duration) -
-                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
-        );
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
